34d0cf2f71364d885298a05ae7d435ccafe66ad1
Steve Barman <steve@stevebarman.com>
2020-08-01 23:00:44 -0400

    Updates

---
+++ precache-manifest.910128ae40183146f64ed371fe2bc7b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f4fdf44245416e9f4eeb0811749bf6",
+    "url": "/tarot/index.html"
+  },
+  {
+    "revision": "35068eff7cda6165ae77",
+    "url": "/tarot/static/css/main.286f35b3.chunk.css"
+  },
+  {
+    "revision": "98dd57c47f5dad4769f0",
+    "url": "/tarot/static/js/2.95b19884.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tarot/static/js/2.95b19884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35068eff7cda6165ae77",
+    "url": "/tarot/static/js/main.29536dde.chunk.js"
+  },
+  {
+    "revision": "014092dfaf27830a3ec6",
+    "url": "/tarot/static/js/runtime-main.0e06f907.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tarot/static/media/logo.5d5d9eef.svg"
+  }
+]);
