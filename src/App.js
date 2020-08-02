5e50e2214136570db0cef30cb89633a5d078693c
Steve Barman <steve@stevebarman.com>
2020-08-01 22:40:18 -0400

    donezo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data.json'
+
+
+const randomCard = () => {
+  return data[Math.floor(Math.random() * data.length)];
+}
 
 function App() {
+  const [card, setCard] = useState(data[0])
+  useEffect(() => {
+    setCard(randomCard())
+  }, [])
+
+
+  const replaceImage = () => {
+    setCard(randomCard())
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{card.title}</h1>
+        <img src={`images/${card.fileName}`} alt={card.title} />
+        <a href={card.link}>Read More</a>
+        <button onClick={() => replaceImage()}>Click Me for a new card</button>
       </header>
     </div>
   );
